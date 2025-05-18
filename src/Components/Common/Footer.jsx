da5e2d362355cfe5c1913d344fb33cc1957f41a7
Saheli-M01 <sahelimondal7845@gmail.com>
2025-05-18 12:03:57 +0530

    Added footer

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,14 +1,19 @@
 import "../../Styles/CommonStyle/_footer.scss";
-const Footer= ()   =>{
+const Footer = () => {
     return (
-    <footer> 
-        <div className="container">
-        <div className="row">
-            <h3>Footer</h3>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-content text-center">
+            <h6>
+              Made with <i className="fas fa-heart heart-icon px-1"></i> by{" "}
+              <span className="creator-name">
+                <a href="https://linktr.ee/Saheli_M">Saheli Mondal</a>
+              </span>&nbsp;
+              |&nbsp; © 2024 <span className="site-name">Visco</span> - Visualizing Algorithms with Passion
+            </h6>
+          </div>
         </div>
-        </div>
-    </footer> 
-
-    )
-}
-export default Footer;
+      </footer>
+    );
+  };
+  export default Footer;
